ee8c2cfd90b504db5338c64abb378c41935c9737
Wiktor <wiktorkalinowski@gmail.com>
2017-08-16 14:19:32 +0200

    Add H2 database

--- src/main/java/com/boot/boot/BootApplication.java
+++ src/main/java/com/boot/boot/BootApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BootApplication.class, args);
-		System.out.println("Hello World");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
 }
